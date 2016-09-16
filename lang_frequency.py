82e80f395c340668474ee58e867e70967732d8cc
Komarov Valentin <komarovvv36@gmail.com>
2016-09-16 17:09:14 +0300

    Refactoring

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,33 +1,36 @@
+from collections import Counter
+
 import operator
 import argparse
 
 
+MOST_COMMON_NUMBER = 5
+
+
 def load_data(filepath):
     with open(filepath, 'r') as file:
-    	return file.read()
+        return file.read()
 
 
 def get_most_frequent_words(text):
-    freq_dict = dict()
     splitted_text = text.split(' ')
+    c = Counter()
     for word in splitted_text:
-    	if word == '' or word == '--':
-    		continue
-    	word = word.translate({ord(c): None for c in '\n[('})
-    	if word not in freq_dict:
-    		freq_dict[word] = 1
-    	else:
-    		freq_dict[word] += 1
-    return freq_dict
+        if word == '' or word == '--':
+            continue
+        word = word.translate(
+            {ord(c): None for c in '!@#$%^&*()[]{};:,./<>?\|`~-=_+\n'}
+        )
+        c[word.lower()] += 1
+    return c
 
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-p', '--path', help='Path to file')
-	args = parser.parse_args()
-	if not args:
-		print('You didn\'t pass \'path\' arguments')
-	text = load_data(args.path)
-	a = get_most_frequent_words(text)
-	sorted_dict = sorted(a.items(), key=operator.itemgetter(1))
-	print(sorted_dict[-1:-10:-1])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--path', help='Path to file', required=True)
+    args = parser.parse_args()
+    if not args:
+        print('You didn\'t pass \'path\' arguments')
+    text = load_data(args.path)
+    a = get_most_frequent_words(text)
+    print(a.most_common(MOST_COMMON_NUMBER))
