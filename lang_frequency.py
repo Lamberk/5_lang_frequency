5486c30cd7704788297228449bd1b24b149225ed
Komarov Valentin <komarovvv36@gmail.com>
2016-09-24 12:39:57 +0300

    Add regexp

--- lang_frequency.py
+++ lang_frequency.py
@@ -2,27 +2,22 @@ from collections import Counter
 
 import operator
 import argparse
+import re
 
 
-MOST_COMMON_NUMBER = 5
+TOP_SIZE = 5
 
 
 def load_data(filepath):
-    with open(filepath, 'r') as file:
-        return file.read()
+    with open(filepath, 'r') as txt_file:
+        return txt_file.read()
 
 
 def get_most_frequent_words(text):
-    splitted_text = text.split(' ')
-    c = Counter()
-    for word in splitted_text:
-        if word == '' or word == '--':
-            continue
-        word = word.translate(
-            {ord(c): None for c in '!@#$%^&*()[]{};:,./<>?\|`~-=_+\n'}
-        )
-        c[word.lower()] += 1
-    return c
+    pattern = re.compile('[^\W\d_]+')
+    parsed_text = pattern.findall(text)
+    counter = Counter(parsed_text)
+    return counter.most_common(TOP_SIZE)
 
 
 if __name__ == '__main__':
@@ -32,5 +27,5 @@ if __name__ == '__main__':
     if not args:
         print('You didn\'t pass \'path\' arguments')
     text = load_data(args.path)
-    a = get_most_frequent_words(text)
-    print(a.most_common(MOST_COMMON_NUMBER))
+    result = get_most_frequent_words(text)
+    print(result)
